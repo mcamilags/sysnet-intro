4d0d0c4cd08c3beeb1773e15340846edc7878558
mcamilags <maria.camila2003@gmail.com>
2025-05-29 22:38:07 -0500

    update

--- fastapi_fargate/app.py
+++ fastapi_fargate/app.py
@@ -7,5 +7,9 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/health")  
+async def health():
+    return {"status": "healthy"}
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
